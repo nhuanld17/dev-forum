1e2a973be5aa13d04090f9604ce99a053d4332c9
TieByn26 <dxuantienn@gmail.com>
2024-10-13 01:30:45 +0700

    build src #1

---
+++ taiwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+import colors from "tailwindcss/colors";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jost: "Jost, sans-serif",
+      },
+      colors: {
+        primary: colors.black,
+      },
+      fontSize: {
+        h1: "56px",
+        h2: "42px",
+        h3: "28px",
+        h4: "21px",
+      },
+    },
+  },
+  plugins: [],
+};
